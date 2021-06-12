b0f2e21d9a477adab9d7837003a36b4a7cce50a4
ProtoByter <admin@protobyte.tech>
2021-06-12 08:05:15 +0100

    First setup of llvm

---
+++ llvm-frontend/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by avalibit on 11/06/2021.
+//
+
+#include "pch.h"
+
+int main() {
+
+}
---
+++ llvm-frontend/pch.h
@@ -0,0 +1,8 @@
+//
+// Created by avalibit on 11/06/2021.
+//
+
+#ifndef RGB4D_PCH_H
+#define RGB4D_PCH_H
+
+#endif //RGB4D_PCH_H
